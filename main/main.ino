6c77b3b1033567310d43c34192d31124f308f819
alonso-herreros <alonso.herreros.c@gmail.com>
2022-04-24 20:50:06 +0200

    Start main.ino file

---
+++ main/main.ino
@@ -0,0 +1,14 @@
+/*
+TODO: Implement BMP280, DHT11 and GPS readings, packing data, and sending it
+both over serial and radio
+*/
+
+
+void setup() {
+    Serial.begin(115200);
+}
+
+
+void loop() {
+    delay(1000);
+}
