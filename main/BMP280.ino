5755d0de3ea384295295268498266aefbf72ed79
alonso-herreros <alonso.herreros.c@gmail.com>
2022-04-24 22:36:54 +0200

    Update BMP280.ino to check if it is the main file

--- main/BMP280.ino
+++ main/BMP280.ino
@@ -3,6 +3,9 @@ TO DO: integrate with the rest of the code to send the data to the server.
 Connect SCL to pin A5 and SDA to pin A4. Power the BMP280 with 5V.
 */
 
+#ifndef MAIN_FILE
+#define MAIN_FILE
+
 // BMP280 pre-setup
 #include <Adafruit_BMP280.h>
 Adafruit_BMP280 bmp; // Use I2C connection
@@ -11,9 +14,6 @@ Adafruit_BMP280 bmp; // Use I2C connection
 #include "NMEA.h"
 char msg[81] = ""; // Buffer for NMEA sentences
 
-
-#ifndef MAIN_FILE
-#define MAIN_FILE
 
 void setup() {
     Serial.begin(115200);
@@ -30,18 +30,6 @@ void loop() {
     delay(1000);
 }
 
-#endif
-
-
-char* read_to_nmea(char *msg) {
-    float temp = bmp.readTemperature();
-    float pres = bmp.readPressure();
-    float alt = bmp.readAltitude(1013.25); // Adjusted to local forecast
-
-    NMEA::mda(msg, pres, temp);
-    return msg;
-}
-
 
 bool start_bmp() {
     // Try to start the BMP280. Print the result (as NMEA text sentence)
@@ -52,3 +40,5 @@ bool start_bmp() {
     );
     Serial.println(msg);
 }
+
+#endif
