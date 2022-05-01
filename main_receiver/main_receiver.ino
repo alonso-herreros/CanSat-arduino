6b85fe21b135d2c541ebf78dac9a22afac9ea9c2
alonso-herreros <alonso.herreros.c@gmail.com>
2022-05-01 18:34:42 +0200

    Add placeholder main_receiver

---
+++ main_receiver/main_receiver.ino
@@ -0,0 +1,24 @@
+/*
+Code created by Alonso Herreros Copete for local CanSat project
+*/
+
+
+#include <RH_RF95.h>
+
+
+#define SERIAL_BAUD 19200 // For wired serial connection
+#define RF95_FREQ 868.0 // For radio connection
+#define RF95_POW 13 // Expected dBm
+
+RH_RF95 rf95; // Will occupy pins 11, 12 and 13
+
+
+void setup() {
+
+}
+
+
+void loop() {
+
+
+}
