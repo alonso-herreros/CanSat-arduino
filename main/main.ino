248c0b193d927a10f2298ac335f6393ea4a8ef8b
alonso-herreros <alonso.herreros.c@gmail.com>
2022-05-01 23:47:12 +0200

    Big update to main.ino. Most things work.

--- main/main.ino
+++ main/main.ino
@@ -1,8 +1,7 @@
 /*
 TODO:
-- Uncomment the code for the dht11 when it's attached to the board
+- (!)Handle disconnection. BMP status checks hang if there's no device connected at all
 - Check radio communication
-- Implement radio receiving - try implementing an instruction for resetting the board
 - Implement some kind of handshake?
 - Maybe switch to a lower level communication protocol (i.e. sending data as bytes)
 
@@ -21,12 +20,12 @@ Code created by Alonso Herreros Copete for local CanSat project
 #define GPS_RX 4 // Blue
 #define GPS_TX 3 // Green
 #define GPS_BAUD 9600 // Can't be changed. This is the module's set baudrate
-#define GPS_TIMEOUT 700 // Timeout for gpsread function
-#define RF95_FREQ 868.0 // For radio connection
-#define RF95_POW 13 // Expected dBm
+#define GPS_TIMEOUT 1100 // Timeout for gpsread function
 #define BMP_ADDR 0x76 // Default address for BMP280
 #define BMP_OK 12 // Code that .getStatus() returns when BMP280 is OK
 #define DHT_PIN A0 // Goes to the data line on the DHT11
+#define RF95_FREQ 868.0 // For radio connection
+#define RF95_POW 13 // Expected dBm
 //define P_AT_SEA = 1013.25; // Should be updated to local forecast. Not in use (GPS)
 
 RH_RF95 rf95; // Will occupy pins 11, 12 and 13
@@ -35,62 +34,94 @@ DHT dht(DHT_PIN, DHT22); // DHT sensor for humidity
 SoftwareSerial gps_ser(GPS_RX, GPS_TX); // SoftwareSerial, check definitions for pins
 
 
-bool rf95_status; // So we don't try communication if it's not working
+bool gps_status;
+bool bmp_status;
+bool dht_status;
+bool rf95_status;
 char msg[NMEA_LEN] = ""; // Buffer for NMEA sentences
 
 
-void (*resetBoard)(void) = 0; // Call this function to reset the board ;)
+void (*restart)(void) = 0; // Call this function to reset the board ;)
 
 
 void setup() {
     Serial.begin(SERIAL_BAUD); // We'll check with (Serial) to see if connection is set up
     gps_ser.begin(GPS_BAUD); // We'll check with .available() to see if GPS is sending data
-    dht.begin(); // Returns void
-    bool bmp_status = bmp.begin(BMP_ADDR); // Returns bool, we'll use that
+    dht.begin(); // We'll check for NaN readings to see if it's working
+    bmp.begin(BMP_ADDR); // Returns bool, but we'll use .getStatus() to see if it's working
     rf95_status = rf95.init(); // Save the result so we don't try sending if it's not working
 
-    rf95.setFrequency(RF95_FREQ);
-    rf95.setTxPower(RF95_POW);
+    gps_status = (gpsread(msg) != NULL); // gpsread return NULL when it times out
+    dht_status = !isnan(dht.readTemperature()); // readTemperature returns NaN if it's not working
+    bmp_status = (bmp.getStatus() == BMP_OK); // This is what we'll use to check BMP status
 
-    delay(1200); // Wait to get some GPS data in buffer, so .available() may return something
+    // (!) for warnings. May want to do something with them later
+    send(NMEA::txt(msg, "Good morning!"));
+    send(NMEA::txt(msg, (Serial) ? "Serial started" : "(!) Serial not working!"));
+    send(NMEA::txt(msg, (gps_status) ? "GPS getting data" : "(!) GPS can't get data!"));
+    send(NMEA::txt(msg, (bmp_status) ? "BMP280 is OK" : "(!) BMP280 is not OK!"));
+    send(NMEA::txt(msg, (dht_status) ? "DHT11 can read" : "(!) DHT11 can't read!"));
+    send(NMEA::txt(msg, (rf95_status) ? "RF95 initialized" : "(!) RF95 failed to initalize!"));
 
-    // [I] for info, [W] for warning. May want to log them to different files later.
-    send(NMEA::txt(msg, (Serial) ? "[I] Serial Started" : "[W] Serial not working!"));
-    send(NMEA::txt(msg, (gps_ser.available()) ? "[I] GPS    Started" : "[W] GPS    not working!"));
-    // Add some check that dht is working??
-    send(NMEA::txt(msg, (bmp_status) ? "[I] BMP280 Started" : "[W] BMP280 unable to start!"));
-    send(NMEA::txt(msg, (rf95_status) ? "[I] RF95   Started" : "[W] RF95   unable to start!"));
+    if (rf95_status) {
+        rf95.setFrequency(RF95_FREQ);
+        rf95.setTxPower(RF95_POW);
+    }
 };
 
 
 void loop() {
+    // If we get a 0x7F, wich is [DEL] in ascii, reset the board
+    while (Serial.available()) {
+        if (Serial.read() == 0x7F) {
+            send(NMEA::txt(msg, "Got reset instruction. Resetting board..."));
+            restart();
+        }
+    }
+
+
     // BMP280 READING
-    float pres = bmp.readPressure();
-    float temp = bmp.readTemperature();
-    //float alt = bmp.readAltitude(P_AT_SEA);
-
-    if (pres!=pres || temp!=temp) { // If both values are NaN, then the BMP280 is not working
-        bmp.begin(BMP_ADDR); // Try to restart the BMP280
-        send(NMEA::txt(msg, "[W] BMP280 unable to read! Trying to restart it."));
+    if (bmp_status != (bmp.getStatus() == BMP_OK)) { // If status changed, update and notify
+        bmp_status = !bmp_status; // This is a bit... eh. Should we just read the status again?
+        send(NMEA::txt(msg, (bmp_status) ? "BMP280 is OK" : "(!) BMP280 is not OK!!"));
     }
 
-    /* DHT11 READING
-    float hum = dht.readHumidity();
-
-    if (hum!=hum) { // If the value is NaN, then the DHT11 is not working
-        dht.begin(); // Try to restart the DHT11
-        send(NMEA::txt(msg, "[W] DHT11 unable to read! Trying to restart it."));
+    float pres = NAN, temp = NAN; //alt = NAN;
+    if (bmp_status) { // If BMP280 is working, read data
+        pres = bmp.readPressure(); // Pa
+        temp = bmp.readTemperature(); // C
+        //alt = bmp.readAltitude(P_AT_SEA); // m
     }
-    */
+
+
+    // DHT11 READING
+    if (dht_status != !isnan(dht.readHumidity())) { // If status changed, update and notify
+        dht_status = !dht_status; // Again, this is't great. Should we update to the actual check?
+        send(NMEA::txt(msg, (dht_status) ? "DHT11 can read" : "(!) DHT11 can't read!!"));
+    }
+
+    float hum = NAN;
+    if (dht_status) // If the DHT11 is working, read data
+        hum = dht.readHumidity(); // %
+
 
     // SEND DATA FROM BMP280 AND DHT11
-    send(NMEA::mda(msg, pres, temp));
-    //send(NMEA::mda(msg, pres, temp, 0, hum));
+    send(NMEA::mda(msg, pres, temp, 0, hum));
     
 
     // GPS READING AND RELAYING
-    while (gps_ser.available()) //Read and relay all available data from GPS
-        send(gpsread(msg));
+    if (gps_status != (gpsread(msg) != NULL)) { // If status changed, update and notify
+        gps_status = !gps_status; // This again. Maybe we should update based on an actual check.
+        NMEA::txt(msg, (gps_status) ? "GPS getting data" : "(!) GPS can't get data!!")
+        if (!gps_status) // Only send it if it broke. If it's ok, it will send it on its own
+            send(msg);
+    }
+
+    if (gps_status) { // If the GPS is working, read and relay data
+        send(msg); // First message is already in the buffer from when we checked
+        while (gps_ser.available()) // Read and relay all available data from GPS
+            send(gpsread(msg));
+    }
 
     delay(1000);
 }
@@ -98,53 +129,41 @@ void loop() {
 
 // buf is going to store an NMEA sentece so it must be NMEA_LEN long!!!
 char* gpsread(char *buf) {
-    unsigned short i = -1; // Index for buffer chars. Won't work until $ is found
-
-    for (unsigned long start = millis(); millis() - start < GPS_TIMEOUT;) {
+    start:
+    uint8_t i = -1; // Index for buffer chars. Won't work until $ is found
+    for (uint32_t start = millis(); millis() - start < GPS_TIMEOUT;) {
         while (gps_ser.available()) { // While there is data to be read
             char c = gps_ser.read(); // Get a character from the GPS serial
             switch (c) {
-            case '\n': // EOL: terminate reading
-                goto end;
-            case '\r': // Discard the carriage return
+            case '\n': // EOL: Terminate reading
+                buf[(i < NMEA_LEN)? i : 0] = '\0'; // Add null terminator
+                return buf; // Return the buffer
+            case '\r': // Discard the carriage returns
                 continue;
-            case '$': // Start of a new sentence: now we can start writing to the buffer
+            case '$': // Start of a sentence: now we can start writing to the buffer
                 i = 0;
             default: // If there's space, add character to buffer
                 if (i < NMEA_LEN - 1)
                     buf[i++] = c;
-
-                else {
-                    i = 0;
-                    goto end;
-                    // Sometimes we get lines that are bigger than the buffer.
-                    // This happens when a new sentence starts being received before the end
-                    // of the original line is reached, resulting in a line that is too long.
-                    // This data isn't valid, so we'll discard it.
-                }
+                else // If the sentence is longer than expected, that's a bad reading
+                    goto start;
             }
         }
     }
-    end:
-    buf[i] = '\0'; // Null-terminate the string
-    return buf;
+    //bad: // If we get here, we timed out or got a bad reading
+    buf[0] = '\0'; // Make the given buffer a 0-length string
+    return NULL; // Return null so we can check how it went
 }
 
 
 // Send the message over both serial and radio
-char* send(char *msg) {
-    // Move the contents to a clean ntca of known length, just in case
-    char buf[NMEA_LEN] = ""; 
-    strncpy(buf, msg, sizeof(buf));
+void send(char *data) {
+    strncpy(msg, data, NMEA_LEN); // Move the contents to our global buffer of known length
+    
+    Serial.println(msg); // Print it to serial for logging
 
-    // Print it to serial for logging
-    Serial.println(buf);
-
-    // If radio is working, send it over radio
-    if (rf95_status) {
-        rf95.send((uint8_t *)buf, sizeof(buf));
+    if (rf95_status) { // If radio is working, send it over radio
+        rf95.send((uint8_t *)msg, NMEA_LEN);
         rf95.waitPacketSent();
     }
-
-    return msg;
 }
