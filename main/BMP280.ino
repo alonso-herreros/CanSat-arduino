fba9a529b7e4d345e335f710c0d9a2b60d3569fe
alonso-herreros <alonso.herreros.c@gmail.com>
2022-05-02 15:57:27 +0200

    Fix BMP280.ino running when it wasn't supposed to

--- main/BMP280.ino
+++ main/BMP280.ino
@@ -3,8 +3,9 @@ TO DO: integrate with the rest of the code to send the data to the server.
 Connect SCL to pin A5 and SDA to pin A4. Power the BMP280 with 5V.
 */
 
-#ifndef MAIN_FILE
-#define MAIN_FILE
+// Don't run any of this is there's another main script.
+#ifndef SERIAL_BAUD
+#define SERIAL_BAUD 19200
 
 // BMP280 pre-setup
 #include <Adafruit_BMP280.h>
